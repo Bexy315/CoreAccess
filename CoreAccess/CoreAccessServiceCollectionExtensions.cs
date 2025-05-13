9f816047c0d3d9d3fef0e359dab178b5d3923271
Felix Zieten <felixzieten16@gmail.com>
2025-05-13 13:58:46 +0200

    Enhance project structure and add sample data for CoreAccess.SampleAPI

--- CoreAccess/CoreAccessServiceCollectionExtensions.cs
+++ CoreAccess/CoreAccessServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.ApplicationParts;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreAccess;
